5b87926e9bf234c3526762a46963ece2140d8ebb
Thiago Favacho R <thiagofr89@hotmail.com>
2023-01-31 23:50:39 -0300

    Atibuicao

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -2,9 +2,7 @@ public class Operadores {
     public static void main(String[] args) {
         int numero = 5;
 
-        numero = - numero;
-
-        numero = numero * -1    ;
+        numero = numero +2;
 
         System.out.println(numero);
 
