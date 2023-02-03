95145e209f82c2b5965b8c0e970f10c1742b1a29
Thiago Favacho R <thiagofr89@hotmail.com>
2023-02-02 22:07:36 -0300

    Operadores Aritimeticos e Concatenacao

---
+++ tipos-variaveis/src/Operadores1.java
@@ -0,0 +1,52 @@
+public class Operadores1 {
+    public static void main(String[] args) {
+       //Operadores aritiméticos
+
+        double soma = 10.5 + 15.7;
+        int subtracao = 113 - 25;
+        int multiplicacao = 20 * 7;
+        float divisao = 15 / 3;
+        int modulo = 18 % 3;
+        double resultado = (10 * 7) + (20/4);
+
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(multiplicacao);
+        System.out.println(divisao);
+        System.out.println(modulo);
+        System.out.println(resultado);
+        
+        
+        //Concatenação (junção de palavras ou números)
+        String concatenacao = "?";
+
+        concatenacao = 1 + 1 + 1 + "1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + 1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + (1+1+1+1);
+        System.out.println(concatenacao);
+        
+        
+        
+        
+        
+        
+        //int a, b;
+
+        //a = 6;
+        //b = 6;
+
+        //String resultado = a==b ?"Verdadeiro" : "Falso";
+
+        //if(a==b)
+            //resultado = "verdadeiro";
+        //else
+            //resultado = "falso";
+
+        //System.out.println(resultado);
+
+    }
+}
