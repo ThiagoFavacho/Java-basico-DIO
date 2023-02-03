c95e2b45926db4b53cb49f4bb240e6cb75f3b47e
Thiago Favacho R <thiagofr89@hotmail.com>
2023-02-02 22:25:57 -0300

    Operador unario

---
+++ tipos-variaveis/src/Operadores2.java
@@ -0,0 +1,14 @@
+public class Operadores2 {
+
+    public static void main(String[] args) {
+    //Operador unário de valor positivo (+)
+    int numero = 5;
+
+    numero = - numero; //convertendo o numero 5 para negativo
+    System.out.println(numero);
+
+    numero = numero * -1; //covertendo o numero 5 para positivo de novo
+    System.out.println(numero);
+
+    }
+}
