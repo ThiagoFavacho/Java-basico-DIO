debc8e010db56ba9dc0431e4e0eb79424fa9793a
Thiago Favacho R <thiagofr89@hotmail.com>
2023-02-23 17:16:09 -0300

    Commit - exercícios_métodos

---
+++ MetodosJava_04/src/Main.java
@@ -0,0 +1,25 @@
+package io.github.jiangdequan;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        //Calculadora
+        System.out.println("Exercício calculadora:");
+        Calculadora.soma(3, 6);
+        Calculadora.subtracao(9, 1.8);
+        Calculadora.multiplicacao(7, 8);
+        Calculadora.divisao(5, 2.5);
+
+        //Mensagem
+        System.out.println("\nExercício mensagem:");
+        Mensagem.obterMensagem(9);
+        Mensagem.obterMensagem(14);
+        Mensagem.obterMensagem(1);
+
+        //Mensagem
+        System.out.println("\nExercício empréstimo:");
+        Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());//Chamando método usando outro método como parâmetro
+        Emprestimo.calcular(1000, Emprestimo.getTresParcelas());
+        Emprestimo.calcular(1000, 5);
+    }
+}
